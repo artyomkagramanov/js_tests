f6fe71fb764ca5cd6e2d96e8ffdecc574fdbcb92
Artyom <artyomkagramanov@gmail.com>
2015-11-11 13:13:25 +0400

    finish js test

--- app/count.js
+++ app/count.js
@@ -3,25 +3,22 @@ exports = (typeof window === 'undefined') ? global : window;
 exports.countAnswers =  {
   count : function (start, end) {
 	
-	var intervalID = window.setInterval(function(){
-		if(start<=end){
-			start++;	      
-        }
-		else {
-			stop(); 
-         }
-  		}, 100);;
+   var timeout;
+   function counter () {
+     
+     if (start <= end) {
+        console.log(start++);
+       timeout = setTimeout(counter, 100);
+     }
+   }
 
-	function stop(){
-  		clearInterval(intervalID);
-  	}
+   counter();
 
-  	return{
-  		cancel:function(){
-  			clearInterval(intervalID);
-  		}
-  	}
-
+   return {
+       cancel : function(){
+           timeout && clearTimeout(timeout);
+       }
+   }
 
 
   }
